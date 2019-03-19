bea54193577bbe48f5ba274a539c12b33eb2478f
Jatin Anand <31433847+Jatin345Anand@users.noreply.github.com>
2019-03-19 14:12:08 +0530

    py

---
+++ jatinbasics/functions/functions.py
@@ -0,0 +1,7 @@
+# from odo.chunks import chunks
+
+
+def handle_uploaded_file(f):
+    with open('ravibasics/static/upload/'+f.name,'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
